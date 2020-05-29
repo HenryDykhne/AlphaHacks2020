31e189b74ad457efaa3b2e5380109327b49fb36e
Henry Dykhne <edykhne@uoguelph.ca>
2020-05-29 19:59:17 -0400

    Added files for db and skeleton forms for listing creation

--- server/app.js
+++ server/app.js
@@ -4,10 +4,15 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./vcLinkDB.db');
 
 app.use(express.static(publicDirectoryPath,{
     extensions: ['html']
 }));
+
+app.
 
 app.listen(PORT, () => {
     console.log('server is up on port: ' + PORT)
