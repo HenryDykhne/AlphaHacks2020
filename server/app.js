8c5f999fe44007d3309e03388bacb20a85221b8d
Henry Dykhne <edykhne@uoguelph.ca>
2020-05-29 20:42:58 -0400

    minor changes for running from command line

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
-const sqlite3 = require('sqlite3').verbose();
+//const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./vcLinkDB.db');
+//const db = new sqlite3.Database('./vcLinkDB.db');
 
 app.use(express.static(publicDirectoryPath,{
     extensions: ['html']
