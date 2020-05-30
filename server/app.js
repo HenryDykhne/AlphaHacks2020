7ecd7dd9eff386ab230cf3fbd7ce26c0da92b4ba
Henry Dykhne <edykhne@uoguelph.ca>
2020-05-30 16:34:30 -0400

    sql insertion code added

--- server/app.js
+++ server/app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser  = require('body-parser');
 
 const HTTP_PORT = process.env.PORT || 8080;
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('vcLinkDB.db', (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the database.');
-});
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+
 
 //Call this function after the http server starts listening for requests
 function onHttpStart() {
@@ -24,25 +23,53 @@ app.use(express.static('public'));
 
 //Setup a "route" to listen on the default url path "http://localhost"
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"../public/html/main.html"));
+    res.sendFile(path.join(__dirname, "../public/html/main.html"));
 });
 
-app.get("/listing", (req, res) => {
-    res.sendFile(path.join(__dirname,"../public/html/listing.html"));
+app.get("/ventureSignUp", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/ventureSignUp.html"));
 });
 
-app.get("/contact", (req, res) => {
-    res.sendFile(path.join(__dirname,"../public/html/contact.html"));
+app.get("/companySignUp", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/companySignUp.html"));
 });
 
-app.post("/insertCompany", (req, res) => {
+app.post("/insertStartup", (req, res) => {
+    let db = new sqlite3.Database('vcLinkDB.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Connected to the database.');
+    });
 
-    return res.send('POST HTTP method on user resource');
+    db.serialize(() => {
+        let insertRequest = "";
+        let startupID = Date.now()
+        insertRequest += `INSERT INTO startup (startup,name,yt_link,email,content)
+        VALUES ("`+startupID+`","`+req.body.name+`","`+req.body.youtube+`","`+req.body.email+`","`+req.body.content+`");`;
+
+        req.body.tags.forEach(tag => insertRequest += `INSERT OR IGNORE INTO startTag (tag_text) VALUES("`+tag+`");`);
+        req.body.tags.forEach(tag => insertRequest += `INSERT OR IGNORE INTO startupTostartTag (startup_id, tag_text) VALUES("`+startupID+`","`+tag+`");`);
+        
+        db.each(insertRequest, (err, row) => {
+            if (err) {
+                console.error(err.message);
+            }
+            //console.log(row.id + "\t" + row.name);
+        });
+    });
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
 });
 
 //404  error page *Always Put Last*
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname,"../public/html/404.html"));
+    res.status(404).sendFile(path.join(__dirname, "../public/html/404.html"));
 });
 
 //setup http server to listen on HTTP_PORT
