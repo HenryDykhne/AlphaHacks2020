b07882bac820d33d4761359f184d2553f230ee1d
kelvin3127 <kelvin3127@users.noreply.github.com>
2020-05-31 04:13:11 -0400

    working the collapsible data

--- public/js/main.js
+++ public/js/main.js
@@ -46,33 +46,36 @@ async function getStartupsMatchTags(){
 
 function myFunction(data) {
 
-  console.log(data)
-  var dataContainer = document.getElementById('showData');
-
-
-    var div = document.createElement('div');
-    div.innerHTML = 'Company Name: ' + data[0].name;
-    dataContainer.appendChild(div);
-
+    document.getElementById("showData").innerHTML = 
+    " <div id='accordian'> " + 
+        "<div class='card'> " +
+          "<div class='card-header' id='headingOne'> " +
+            "<h5 class'mb-0'> " +
+              "<button class='btn btn-link collapsed' data-toggle='collapse' data-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'> " +
+              data[6].name +
+          "</button></h5></div>" +
+          "<div id='collapseOne' class='collapse' aria-labelledby='headingOne' data-parent='#accordion'> " +
+            "<div class='card-body'> " +
+                "<div class='row'> " +
+                    "<div class='col-md-6'>" +
+                        "<p>" +
+                            data[6].content +
+                        "</p>" +
+                        "<p>" +
+                            "Email: " + data[6].email +
+                        "</p>" +
+                    "</div>" +
+                    "<div class='col-md-6'>" +
+                        "<iframe width='560' height='315' src='https://www.youtube.com/embed/gu-trYf96xo' ></iframe>" +
+                    "</div>" +
+                "</div>" +
+          "</div></div> " +
+        "</div>" +
+      "</div>"
+    ;
 
 
 }
 
 
-/*
-  document.getElementById("showData").innerHTML = 
-  " <div id='accordian'> " + 
-      "<div class='card'> " +
-        "<div class='card-header' id='headingOne'> " +
-          "<h5 class'mb-0'> " +
-            "<button class='btn btn-link collapsed' data-toggle='collapse' data-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'> " +
-            "data[i].name" +
-        "</button></h5></div>" +
-        "<div id='collapseOne' class='collapse' aria-labelledby='headingOne' data-parent='#accordion'> " +
-          "<div class='card-body'> " +
-          "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, no  " +
-        "</div></div> " +
-      "</div>" +
-    "</div>"
-  ;
-  */
+
